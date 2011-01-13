51cf17db3c001862ab38ee3d7b69a65b304b6c2e
James Ming <jamesming@gmail.com>
2011-01-12 20:29:25 -0800

    counter

--- system/application/views/home/contactus_view.php
+++ system/application/views/home/contactus_view.php
@@ -22,61 +22,26 @@
 	</div>
 </div>
 
-
+<div class='container '  >
+		
+	<span id='ourteam' href="#fancy_zoom_modal_box_for_text_editor" class='fancy_zoom_modal_box_for_text_editor_link cursor_pointer '  >This is a simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</span>
+	
+</div>
 
 <div   class='container'     style='margin-top:30px'  >
 
-	<div class='column span-2 '  >
+	<div class='column span-14 '   style='text-align:center'   >
 		
-		<img src='<?php echo base_url()    ?>images/woman.jpg'>	
+		<img src='<?php echo base_url()    ?>images/contactUsFormBig.jpg'>	
 		
 		
 		
 	</div>
 			
-	<div class='column span-14 '  >
-			
-		<span id='ourteam' href="#fancy_zoom_modal_box_for_text_editor" class='fancy_zoom_modal_box_for_text_editor_link cursor_pointer '  >This is a simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</span>
-		
-	</div>
 
 	
-	<div class='column span-8 last'   >
+	<div class='column span-10 last'   >
 		
-		<img   style='
-		float:right;
-		width:90%;
-		margin:19px 0px;
-		
-		'  
-		src='<?php echo base_url()    ?>images/address.jpg'>	
-		
-	
-			
-	</div>
-	
-</div>
-
-
-
-<div   class='container'     >
-
-	<div class='column span-2' >&nbsp;
-	</div>
-	<div class='column span-20' style='height:50px;'    >
-		<p  class=' font_color_blue'   style='
-			text-align:center;
-			font-weight:bold;
-			font-size:24px;
-			
-			'  >
-		Contact Us Now
-		</p>
-	</div>	
-	<div class='column span-2 last'>&nbsp;
-	</div>		
-	
-</div>
 <style>
 #form_table td{
 height:40px;
@@ -84,7 +49,7 @@ height:40px;
 .form_style{
 	border:1px solid gray;
 #	font-size:27px;
-	width:294px;
+	width:94px;
 	height:25px;
 	padding-left:10px;
 	border-top-left-radius: 6px;
@@ -102,13 +67,6 @@ height:40px;
 }
 </style>
 <form name='prospects' id='prospects'>
-<div   class='container'     style='padding-bottom:40px'   >
-
-	<div class='column span-2' >&nbsp;
-	</div>
-	<div class='column span-20'>
-		
-		
 		<table id='form_table'>
 			<tr>
 				<td>Your Name
@@ -150,14 +108,16 @@ height:40px;
 				
 			
 		</table>
-		
-	</div>	
+</form>
 
-	<div class='column span-2 last'>&nbsp;
-	</div>		
+			
+	</div>
 	
 </div>
-</form>
+
+
+
+
 
 <?php     
 			$this->load->view('footer/optionone_footer.php');
