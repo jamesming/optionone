b527f8ca6809b71b12a2b7ddc88182e298d64725
James Ming <jamesming@gmail.com>
2011-01-25 12:06:30 -0800

    final

--- system/application/views/edit/footer/optionone_footer.php
+++ system/application/views/edit/footer/optionone_footer.php
@@ -7,7 +7,17 @@
 **/
 
 --> 
+<div   id='footer_sponsor'  href="#fancy_zoom_modal_box_for_text_editor"   class='cursor_pointer fancy_zoom_modal_box_for_text_editor_link container ' style='
 
+padding:20px 20px;
+#height:auto;
+#width:auto;
+#color:gray;
+#background:lightblue;
+
+	
+	'  >
+</div>
 <hr>
 <div   id='footer'  href="#fancy_zoom_modal_box_for_text_editor"   class='cursor_pointer fancy_zoom_modal_box_for_text_editor_link container ' style='
 
