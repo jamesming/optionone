a502387d8cb29deac6c887bf7f907728c525ae2d
James Ming <jamesming@gmail.com>
2011-02-18 12:42:39 -0800

    added pdf

--- system/application/views/edit/home/employment_view.php
+++ system/application/views/edit/home/employment_view.php
@@ -34,10 +34,18 @@
 <div   class='container'       >
 
 
-			
-		<span id='employment_main_div' href="#fancy_zoom_modal_box_for_text_editor" class='fancy_zoom_modal_box_for_text_editor_link cursor_pointer '  >XX</span>
 		
-
+		<div id='employment_main_div'  href="#fancy_zoom_modal_box_for_text_editor"  class=' span-16 fancy_zoom_modal_box_for_text_editor_link  cursor_pointer'  ></div>
+		<div id=''  class='last span-8  '      style='padding-top:30px; vertical-align:top'      >
+			
+			<div   style='height:9px;background:url(<?php echo base_url()    ?>images/bluebox_top.jpg) no-repeat'  >
+			</div>
+			<div  id='employment_pdf_div' href="#fancy_zoom_modal_box_for_text_editor" class='fancy_zoom_modal_box_for_text_editor_link cursor_pointer' style='background:url(<?php echo base_url()    ?>images/bluebox_center.jpg) repeat-y '>
+				
+			</div>
+			<div   style='height:21px;background:url(<?php echo base_url()    ?>images/bluebox_bottom.jpg) no-repeat'  >
+			</div>
+		</div>
 	
 </div>
 
