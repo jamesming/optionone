157bd0e3e579aee1c9d721094fc7452f3b36ae3b
James Ming <jamesming@gmail.com>
2011-03-08 10:41:24 -0800

    added

--- system/application/config/config.php
+++ system/application/config/config.php
@@ -19,7 +19,7 @@ if( $_SERVER['HTTP_HOST'] == 'www.jamesming.com' ){
 	$config['base_url']	= "http://127.0.0.1/optionone";
 }elseif( $_SERVER['HTTP_HOST'] == 'www.ihcoption.com' ){
 	$config['base_url']	= "http://www.ihcoption.com";
-}elseif( $_SERVER['HTTP_HOST'] == 'http://www.optiononecareathome.com' ){
+}elseif( $_SERVER['HTTP_HOST'] == 'www.optiononecareathome.com' ){
 	$config['base_url']	= "http://http://www.optiononecareathome.com";
 };
 
