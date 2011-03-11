41fe80872769d7871359f526666151ec8c22ff44
James Ming <jamesming@gmail.com>
2011-03-11 10:21:04 -0800

    added

---
+++ system/application/views/header/meta_tags.php
@@ -0,0 +1,2 @@
+<meta name="google-site-verification" content="eSm_uzLBiS2nd6aEwYsuQbfZd8iEIRUafjyL4hhCOfI" />
+<title> OptionOne  </title>
--- system/application/views/home/main_view.php
+++ system/application/views/home/main_view.php
@@ -1,12 +1,15 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
+<?php     
+			$this->load->view('header/meta_tags.php');
+?>
 	
 
 
 	<script type="text/javascript" language="Javascript" src = "<?php echo  base_url();   ?>js/jquery.js"></script>
 	
-<head>
+
 <?php     	$this->load->view('header/header_css.php');  ?>
 	<script type="text/javascript" language="Javascript">
 		
