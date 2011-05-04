1eab209cb50506b2ff520773f8f18dd6fa393583
James Ming <jamesming@gmail.com>
2011-05-04 07:47:38 -0700

    added

--- system/application/views/header/optionone_header.php
+++ system/application/views/header/optionone_header.php
@@ -1,3 +1,30 @@
+<script type="text/javascript">
+
+
+
+ var _gaq = _gaq || [];
+
+ _gaq.push(['_setAccount', 'UA-22750353-1']);
+
+ _gaq.push(['_trackPageview']);
+
+
+
+ (function() {
+
+   var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+
+   ga.src = ('https:<https://owa.optiononecareathome.com/owa/UrlBlockedError.aspx>' == document.location.protocol ? 'https://ssl<https://ssl/>' : 'http://www<http://www/>') + '.google-analytics.com/ga.js';
+
+   var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+
+ })();
+
+
+
+</script>
+
+
 <style>
 #myGallery img{
 	margin:6px 0px 0px 0px;
